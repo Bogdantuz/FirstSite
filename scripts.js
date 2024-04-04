4695f465eb5c14fc68271491b3a183807ff3ffaf
BogdanTuz <111379255+Bogdantuz@users.noreply.github.com>
2024-04-03 20:18:56 -0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.querySelector("#download-now button").addEventListener("click", () => {
+    alert("Sorry, but this app isn't already done")
+})
+document.querySelector("#watch-more button").addEventListener("click", () => {
+    alert("Sorry, but this protocol doesn't already exist")
+})
+document.querySelector("#end-download-now button").addEventListener("click", () => {
+    alert("Sorry, but this app isn't already done")
+})
